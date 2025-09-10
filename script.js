ecf468b38171f1c60909b43aecfac768b90cc95a
Danulo4342 <kindijdanilo@gmail.com>
2025-09-10 09:41:26 +0300

    js

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-let age = 14;
-console.log(age);
-let name = "Danulo";
-console.log(name);
-let isStudent = true;
-console.log(isStudent);
-let myString = "hello my name is Danulo";
-console.log(myString);
-let myNumber = 7;
-console.log(myNumber + 10);
-let myNull = null;
-console.log(myNull);
-const userName = prompt('Введіть своє ім’я');
-alert(`${userName}`);
-console.log(typeof userName);
-let userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
-console.log(userAnswer);
-alert("Увага! Дія може бути небезпечною.");
-let txt = confirm("Ви впевнені, що хочете продовжити?");
+
+
+let celsius = "20";
+let fahrenheit = celsius * 1.8;
+console.log(fahrenheit);
+
+let daysInMonth = 31;
+let hoursInMonth = daysInMonth * 24;
+let minutesInMonth = daysInMonth * 24 * 60;
+console.log(daysInMonth, hoursInMonth, minutesInMonth);
+
+let health = 10;
+let energy = 10;
+let healthAfter = health - 3;
+let energyAfter = energy - 2;
+console.log("health", healthAfter, "energy", energyAfter);
+
+let totalPrice = 500;
+let discount = 0.1;
+let discountedPrice = totalPrice * discount;
+console.log(discountedPrice);
+
+const floatNumber = 12.78;
+const roundedDown = Math.floor(floatNumber);
+console.log(roundedDown);
+
+const floatString = "45.67";
+const parsedFloat = (Number.parseFloat(floatString));
+console.log(parsedFloat);
+
+const intString = "123";
+const parsedInt = (Number.parseInt(intString));
+console.log(parsedInt);
+
+const number = 2;
+const sqrtNumber = Math.sqrt(Math.pow(number, 4));
+console.log(sqrtNumber);
